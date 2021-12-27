669e0dcd144c132932cd78aff0b50d911be58f86
matekuna92 <kunamate66@gmail.com>
2021-12-27 13:33:34 +0100

    xIsNext boolean, check which player is the next

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
+// https://reactjs.org/tutorial/tutorial.html#function-components
+function Square(props) {
     return (
       <button
           className="square"
-          onClick={() => this.props.onClick()}>
-          {this.props.value}
+          onClick={props.onClick}>
+          {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
@@ -20,14 +19,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();   // https://reactjs.org/tutorial/tutorial.html#data-change-without-mutation
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
@@ -42,7 +45,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
